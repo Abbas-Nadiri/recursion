e9924b78a956c705a47be8f2faa2c9691961ffff
ahnadiri <108028111+Abbas-Nadiri@users.noreply.github.com>
2025-01-12 14:11:09 +0000

    create recursive merge sort function

--- src/index.js
+++ src/index.js
@@ -8,11 +8,37 @@ function fibs(num) {
   return array;
 }
 
-//console.log(fibs(8));
+console.log(fibs(8));
 
 function fibsRec(num) {
-    if (num == 2) return [0, 1];
-    const sequence = fibsRec(num - 1);
-    return [...sequence, sequence[sequence.length - 1] + sequence[sequence.length - 2]];
+  if (num == 2) return [0, 1];
+  const sequence = fibsRec(num - 1);
+  return [
+    ...sequence,
+    sequence[sequence.length - 1] + sequence[sequence.length - 2],
+  ];
 }
+
 console.log(fibsRec(15));
+
+function mergeSort(array) {
+  if (array.length <= 1) {
+    return array;
+  }
+  const middleIndex = Math.ceil(array.length / 2);
+  const firstHalf = array.slice(0, middleIndex);
+  const secondHalf = array.slice(middleIndex);
+  return merge(mergeSort(firstHalf), mergeSort(secondHalf));
+}
+
+function merge(leftArray, rightArray) {
+  const mergedArray = [];
+  while (leftArray.length > 0 && rightArray.length > 0) {
+    leftArray[0] < rightArray[0]
+      ? mergedArray.push(leftArray.shift())
+      : mergedArray.push(rightArray.shift());
+  }
+  return [...mergedArray, ...leftArray, ...rightArray];
+}
+
+console.log(mergeSort([3, 2, 4, 5, 1, 6]));
